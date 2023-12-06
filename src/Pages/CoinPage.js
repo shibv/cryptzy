3735b4a95e70f68c0cbaad860e980b7e8fe458d1
shiv <ps431352@gmail.com>
2023-12-06 15:30:18 +0530

    .

--- src/Pages/CoinPage.js
+++ src/Pages/CoinPage.js
@@ -1,70 +1,19 @@
 import React, { useEffect, useState } from 'react'
+import './CoinPage.css'
 import { useParams } from 'react-router-dom'
 import {CryptoState} from '../CryptoContext'
 import axios from 'axios';
 import { SingleCoin } from '../Config/api';
-import { makeStyles } from "@mui/styles";
 import CoinInfo from '../Components/CoinInfo';
 import { Typography } from '@mui/material';
 import { numberWithCommas } from "../Components/CoinsTable"
  import { LinearProgress } from "@mui/material" ;
 
+
 // css
-const useStyles = makeStyles((theme ) => ({
-  container:{
-    display:'flex',
-    '@media (max-width: 600px)': {
-      flexDirection:"column",
-      alignItems:"center"
-    },
-  },
-  sidebar:{
-    width: "30%",
-    '@media (max-width: 600px)':{
-      width : "100%",
-    },
-    display: "flex",
-    flexDirection : "column",
-    alignItems : "center",
-    marginTop : 25,
-    borderRight : "2px solid grey  "
 
-  },
-  heading : {
-     fontWeight: "bold",
-     marginBottom : 20, 
-     fontFamily  : " Montserrat",
-  },
-  desciption : {
-    width : "100%",
-    fontFamily: "Montserrat ",
-    padding: 25,
-    paddingBottom : 15,
-    paddingTop : 0 ,
-    textAlign : " justify ", 
 
-  },
-  marketdata : {
-    alignSelf : "start", 
-    padding : 25 ,
-    paddingTop : 10, 
-    width: "100%", 
-    '@media (min-width: 768px) and (max-width: 1024px)':{
-      display : "flex",
-      justifyContent: "space-around"
-    },
-    '@media (min-width: 481px) and (max-width: 786px)':{
-      flexDirection : "column",
-      alignItems : "center"
-    },
-    '@media (max-width: 480px) ':{
-      alignItems:"start"
-    },
-
-  }
-}));
-
-const CoinPage = ( props) => {
+const CoinPage = ( ) => {
 
   const { id } = useParams();
 
@@ -81,27 +30,27 @@ const CoinPage = ( props) => {
   useEffect(() =>{
     fetchCoin()
   }, [])
-  const classess = useStyles(props);
+ 
   if (!coin) return <LinearProgress style={{ backgroundColor: "aqua" }} />;
   return (
-    <div className={classess.container}>
-       <div className={classess.sidebar}>
+    <div className='container'>
+       <div className='sidebar'>
          
             <img src={coin?.image.large} alt={coin?.name} height="200" style={{marginBottom : 20}} />
-            <Typography variant='h3' className={classess.heading}  style={{fontWeight: 'bold'}} >{coin?.name}</Typography>
-            <Typography variant='subtitle1' className={classess.desciption}>
+            <Typography variant='h3' className='heading'  style={{fontWeight: 'bold'}} >{coin?.name}</Typography>
+            <Typography variant='subtitle1' className='desciption'>
                 {coin?.description.en.split(". ")[0]}.
             </Typography>
-            <div className={classess.marketdata}>
+            <div className='marketdata'>
                 <span style={{ display : 'flex'}}>
-                   <Typography variant='h5' className={classess.heading}  style={{fontWeight: 'bold'}}>Rank:</Typography>
+                   <Typography variant='h5' className='heading' style={{fontWeight: 'bold'}}>Rank:</Typography>
                    &nbsp;
                    <Typography variant='h5' style={{fontFamily : 'Montserrat'}}>
                    {numberWithCommas(coin?.market_cap_rank)}
                    </Typography>
                 </span>
                 <span style={{ display : 'flex'}}>
-                   <Typography variant='h5' className={classess.heading}  style={{fontWeight: 'bold'}}>Current Price:</Typography>
+                   <Typography variant='h5' className='heading'  style={{fontWeight: 'bold'}}>Current Price:</Typography>
                    &nbsp;
                    <Typography variant='h5' style={{fontFamily : 'Montserrat'}}>
                    {symbol}{" "}
@@ -111,7 +60,7 @@ const CoinPage = ( props) => {
                    </Typography>
                 </span>
                 <span style={{ display : 'flex'}}>
-                   <Typography variant='h5' className={classess.heading} style={{fontWeight: 'bold'}}>Market Cap:</Typography>
+                   <Typography variant='h5' className='heading' style={{fontWeight: 'bold'}}>Market Cap:</Typography>
                    &nbsp; &nbsp;
             <Typography
               variant="h5"
